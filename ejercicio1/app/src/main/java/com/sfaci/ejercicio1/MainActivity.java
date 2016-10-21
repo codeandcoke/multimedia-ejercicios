8d7d1cf1ba23b6fb2d36640eb6786ad67cf9724f
Santiago Faci <santiago.faci@mail.com>
2016-10-21 20:57:43 +0200

    [Ejercicio1] Ya se pueden eliminar y marcar como hechas las tareas

--- ejercicio1/app/src/main/java/com/sfaci/ejercicio1/MainActivity.java
+++ ejercicio1/app/src/main/java/com/sfaci/ejercicio1/MainActivity.java
@@ -2,7 +2,10 @@ package com.sfaci.ejercicio1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,6 +29,10 @@ public class MainActivity extends AppCompatActivity implements
 
         Button btAnadir = (Button) findViewById(R.id.btAnadir);
         btAnadir.setOnClickListener(this);
+        Button btPendientes = (Button) findViewById(R.id.btPendientes);
+        btPendientes.setOnClickListener(this);
+        Button btHechas = (Button) findViewById(R.id.btHechas);
+        btHechas.setOnClickListener(this);
 
         tareas = new HashMap<>();
 
@@ -34,8 +41,45 @@ public class MainActivity extends AppCompatActivity implements
         adaptador = new ArrayAdapter<>(
                 this, android.R.layout.simple_list_item_1, listaTareas);
         lvTareas.setAdapter(adaptador);
+
+
+        registerForContextMenu(lvTareas);
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_contextual, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo info =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int posicion = info.position;
+
+        String nombreTareaSeleccionada = null;
+
+        switch (item.getItemId()) {
+            case R.id.opcion_eliminar:
+                nombreTareaSeleccionada = listaTareas.get(posicion);
+                listaTareas.remove(posicion);
+                tareas.remove(nombreTareaSeleccionada);
+                adaptador.notifyDataSetChanged();
+                return true;
+            case R.id.opcion_hecho:
+                nombreTareaSeleccionada = listaTareas.get(posicion);
+                listaTareas.remove(posicion);
+                Tarea tarea = tareas.get(nombreTareaSeleccionada);
+                tarea.hacer();
+                adaptador.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
 
     @Override
     public void onClick(View view) {
